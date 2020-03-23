ed72e76517a51afb1ab1d0ccbf971d53e7046a27
unknown <essudheer@gmail.com>
2020-03-23 11:51:49 +0000

    testing

--- src/org/prolian/scm/SecondClass.java
+++ src/org/prolian/scm/SecondClass.java
@@ -2,6 +2,8 @@ package org.prolian.scm;
 
 public class SecondClass {
 
-    
+    public static void main(String[] args) {
+        System.out.println("second testing");
+    }
 
 }
--- src/org/prolian/scm/scmetest.java
+++ src/org/prolian/scm/scmetest.java
@@ -3,5 +3,6 @@ package org.prolian.scm;
 public class scmetest {
     public static void main(String[] args) {
 
+        System.out.println("I love Git");
     }
 }
